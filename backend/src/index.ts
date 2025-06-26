c342902d0e980a093e0585ec2d4700ed979bab74
swastikCommits <textswastik.alt@gmail.com>
2025-06-26 16:34:07 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app
