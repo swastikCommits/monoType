83b05e24f9dd6c3cc3a59628aa4846639e3780f2
swastikCommits <textswastik.alt@gmail.com>
2025-06-29 22:44:18 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/signup" element={<Signup/>} />
+        <Route path = "/signin" element={<Signin/>} />
+        <Route path = "/blog" element={<Blog/>} />
+      </Routes>
+
+
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
