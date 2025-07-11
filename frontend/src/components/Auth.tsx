535561d510f3d14e698519fbfca17fb9c0959a25
swastikCommits <textswastik.alt@gmail.com>
2025-07-11 10:41:03 +0530

    init

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,14 +1,28 @@
 import  type { ChangeEvent} from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import type { SignupInput }  from "@daddyonyx/monotype-common";
+import axios  from "axios";
+import { BACKEND_URL } from "../config";
 
 
 export const Auth = ({ type } : { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
         email: "",
         password: ""
     });  
+    
+    async function sendRequest() {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs");
+        } catch(e) {
+            alert(e);
+        }
+    }
     
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -37,7 +51,7 @@ export const Auth = ({ type } : { type: "signup" | "signin" }) => {
                             password: e.target.value
                         })
                     }} />
-                    <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                    <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
         </div>
